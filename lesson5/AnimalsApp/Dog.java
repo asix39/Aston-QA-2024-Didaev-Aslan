b3ed251ddef87faf7a974cca357f364f0bd66bb2
asix39 <000aslan5@gmail.com>
2024-11-12 13:59:38 +0200

    Delete lesson5 directory

--- lesson5/AnimalsApp/Dog.java
+++
@@ -1,9 +0,0 @@
-package lesson5.AnimalsApp;
-
-public class Dog extends Animal {
-    public static int count = 0;
-    public Dog(String name, int runLimit, int swimLimit) {
-        super(name, runLimit, swimLimit);
-        count++;
-    }
-}
